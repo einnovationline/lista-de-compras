03883a4488dae7f1d0e227c1e333ae07f131526d
hnhostins <hnhostins@gmail.com>
2023-03-11 20:36:14 -0300

    ajuste

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import { useState } from 'react';
 
+const products = [
+  {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
+  {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
+  {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
+  {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
+  {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
+  {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
+];
+
+function App() {
+  return <FilterableProductTable products={products} />;
+}
+
 function FilterableProductTable({ products }) {
   const [filterText, setFilterText] = useState('');
   const [inStockOnly, setInStockOnly] = useState(false);
@@ -86,12 +99,7 @@ function ProductTable({ products, filterText, inStockOnly }) {
   );
 }
 
-function SearchBar({
-  filterText,
-  inStockOnly,
-  onFilterTextChange,
-  onInStockOnlyChange
-}) {
+function SearchBar({filterText,inStockOnly,onFilterTextChange,onInStockOnlyChange}) {
   return (
     <form>
       <input 
@@ -104,21 +112,10 @@ function SearchBar({
           checked={inStockOnly} 
           onChange={(e) => onInStockOnlyChange(e.target.checked)} />
         {' '}
-        Only show products in stock
+        Only show products in stock/produtos com estoque
       </label>
     </form>
   );
 }
 
-const PRODUCTS = [
-  {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
-  {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
-  {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
-  {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
-  {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
-  {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
-];
-
-export default function App() {
-  return <FilterableProductTable products={PRODUCTS} />;
-}
+export default App
